c8a8e79dc3a1719c719bc14222c229d461790fd3
Igor <igoramarals96@gmail.com>
2021-07-22 13:56:35 -0300

    commit

---
+++ function.js
@@ -0,0 +1,74 @@
+const itens = [
+    {
+        id: 0,
+        nome: 'calçados',
+        img: media/camisa.png,
+        quantidade: 0,
+    },
+    {
+        id: 0,
+        nome: 'camisas',
+        img: media/camisa.png,
+        quantidade: 0,
+    },
+    {
+        id: 0,
+        nome: 'blusas',
+        img: media/camisa.png,
+        quantidade: 0,
+    },
+    {
+        id: 0,
+        nome: 'casacos',
+        img: media/camisa.png,
+        quantidade: 0,
+    },
+    {
+        id: 0,
+        nome: 'saias',
+        img: media/camisa.png,
+        quantidade: 0,
+    },
+    {
+        id: 0,
+        nome: 'vestidos',
+        img: media/camisa.png,
+        quantidade: 0,
+    },
+    {
+        id: 0,
+        nome: 'calças',
+        img: media/camisa.png,
+        quantidade: 0,
+    },
+    {
+        id: 0,
+        nome: 'meias',
+        img: media/camisa.png,
+        quantidade: 0,
+    },
+    {
+        id: 0,
+        nome: 'oculos',
+        img: media/camisa.png,
+        quantidade: 0,
+    },
+    {
+        id: 0,
+        nome: 'colares',
+        img: media/camisa.png,
+        quantidade: 0,
+    },
+    {
+        id: 0,
+        nome: 'cintos',
+        img: media/camisa.png,
+        quantidade: 0,
+    },
+    {
+        id: 0,
+        nome: 'brincos',
+        img: media/camisa.png,
+        quantidade: 0,
+    },
+]
